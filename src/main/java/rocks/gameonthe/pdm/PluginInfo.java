2fe9303bb102c45f8e619b5c605b0ce19e79612d
Mohron <mohron@mohron.net>
2018-02-08 01:36:21 -0600

    Setup gradle and plugin info

---
+++ src/main/java/rocks/gameonthe/pdm/PluginInfo.java
@@ -0,0 +1,14 @@
+package rocks.gameonthe.pdm;
+
+public final class PluginInfo {
+
+  public static final String ID = "personaldimanager";
+  public static final String NAME = "@NAME@";
+  public static final String VERSION = "@VERSION@";
+  public static final String DESCRIPTION = "@DESCRIPTION@";
+  public static final String AUTHORS = "Mohron";
+  public static final String SPONGE_API = "7.0.0";
+  public static final String GP_VERSION = "4.3.0";
+  public static final double GP_API_VERSION = 0.8;
+  public static final String NUCLEUS_VERSION = "1.2.0";
+}
